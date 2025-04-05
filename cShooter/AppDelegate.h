47400f870751daa677a12e9b52fdf75c40580618
Noah Lin <nglin34@gmail.com>
2025-04-05 18:50:15 -0400

    Initial Commit

---
+++ cShooter/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  cShooter
+//
+//  Created by Noah Lin  on 2025-04-05.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ cShooter/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  cShooter
+//
+//  Created by Noah Lin  on 2025-04-05.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
